0a1e00b6cb6244b73597cac40d1f4a09969a9e5a
Gavrilov-Val <gavrilov.valery@mail.ru>
2025-02-13 04:59:09 -0700

    add brain-even file and command

---
+++ bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import askName from '../src/even.js';
+
+console.log('Welcome to the Brain Games!');
+askName();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
