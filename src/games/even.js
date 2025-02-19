2d5c59eb26ccfaedf48bb894e86baa461a419808
Gavrilov-Val <gavrilov.valery@mail.ru>
2025-02-18 18:01:55 -0700

    improve brain-even

--- src/games/even.js
+++ src/games/even.js
@@ -1,33 +1,16 @@
-import readlineSync from 'readline-sync';
+import runGame, { getRandomNumber } from '../index.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
 
-const getRandomNumber = () => Math.floor(Math.random() * 100);
-
-const evenGame = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  let correctAnswers = 0;
-  const requiredAnswers = 3;
-
-  while (correctAnswers < requiredAnswers) {
-    const randomNumber = getRandomNumber();
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-      correctAnswers += 1;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
-
-  console.log(`Congratulations, ${name}!`);
+const generateQuestionAndAnswer = () => {
+  const number = getRandomNumber(1, 100);
+  const question = String(number);
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
 
-export default evenGame;
+const startEvenGame = () => runGame(description, generateQuestionAndAnswer);
+
+export default startEvenGame;
