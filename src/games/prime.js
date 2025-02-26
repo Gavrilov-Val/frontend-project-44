9ccbe7c19166e62e71022879710e36582d5a5cbe
Gavrilov-Val <gavrilov.valery@mail.ru>
2025-02-26 10:17:05 -0700

    fix brain-prime code

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,9 +2,22 @@ import runGame, { getRandomNumber } from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-// eslint-disable-next-line max-len
-const primeNumArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-const isPrime = (number) => primeNumArr.includes(number);
+const divisorsList = (num) => {
+  const divisors = [];
+  let divisor = 2;
+  let number = num;
+  while (number >= 2) {
+    if (number % divisor === 0) {
+      divisors.push(divisor);
+      number /= divisor;
+    } else {
+      divisor += 1;
+    }
+  }
+  return divisors;
+};
+
+const isPrime = (number) => divisorsList(number).length === 1;
 
 const generateQuestionAndAnswer = () => {
   const number = getRandomNumber(1, 100);
