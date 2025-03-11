86676a3e9708f82e3e3f481186f4c698318795ee
Gavrilov-Val <gavrilov.valery@mail.ru>
2025-03-11 16:12:10 -0600

    delete unnecessary return on line 21

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ const runGame = (description, generateQuestionAndAnswer) => {
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
-      return;
     }
   }
 
