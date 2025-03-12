75ec1b94bd1a6c235ec47c864c28583f4bb2834a
Gavrilov-Val <gavrilov.valery@mail.ru>
2025-03-12 06:47:22 -0600

    fix eslint issues

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,14 +3,14 @@ import getRandomNumber from '../utils.js';
 
 const description = 'What number is missing in the progression?';
 
+const makeProgression = (start, step, length) => Array.from({ length }, (_, i) => start + step * i);
+
 const generateQuestionAndAnswer = () => {
   const startNumber = getRandomNumber(1, 10);
   const increment = getRandomNumber(1, 10);
   const hiddenNumber = getRandomNumber(1, 10);
   const progressionLength = 10;
-  const progressionArray = Array.from(
-    { length: progressionLength }, (start, i) => startNumber + (increment * i)
-  );
+  const progressionArray = makeProgression(startNumber, increment, progressionLength);
   const correctAnswer = progressionArray[hiddenNumber - 1].toString();
   progressionArray[hiddenNumber - 1] = '..';
   const question = progressionArray.join(' ');
